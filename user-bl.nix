7867c783695fdd9be4473c0e3007486af970d523
blank71 <8blank71@gmail.com>
2023-10-21 07:04:02 +0900

    add: pstree

--- user-bl.nix
+++ user-bl.nix
@@ -11,11 +11,13 @@
       gnome-extension-manager
       gnome.pomodoro
       gnome.gnome-tweaks
+      #gnomeExtensions.gtk-title-bar
       gnomeExtensions.just-perfection
       google-chrome
       google-drive-ocamlfuse
       #mattermost-desktop
       opam
+      pstree
       remmina
       slack
       vscode
