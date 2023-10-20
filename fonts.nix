a5856674d8bade7db7f6321c6f7d43d315d77c10
blank71 <8blank71@gmail.com>
2023-10-21 07:01:49 +0900

    fix: tofu

--- fonts.nix
+++ fonts.nix
@@ -6,6 +6,7 @@
     packages = with pkgs; [
       fira-code
       hackgen-font
+      noto-fonts
       noto-fonts-cjk-serif
       noto-fonts-cjk-sans
       noto-fonts-emoji
@@ -17,8 +18,8 @@
     fontDir.enable = true;
     fontconfig = {
       defaultFonts = {
-        serif = ["Noto Serif CJK JP" "Noto Color Emoji"];
-        sansSerif = ["Noto Sans CJK JP" "Noto Color Emoji"];
+        serif = ["Noto Serif CJK JP" "Noto Serif" "Noto Color Emoji"];
+        sansSerif = ["Noto Sans CJK JP" "Noto Sans" "Noto Color Emoji"];
         monospace = ["HackGen Console" "JetBrainsMono Nerd Font" "Noto Color Emoji"];
         emoji = ["Noto Color Emoji"];
       };
